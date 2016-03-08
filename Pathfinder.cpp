47d03831e7084bc8adddca59e30258925876ef67
Maddie <mk186442@falmouth.ac.uk>
2016-03-08 10:48:14 +0000

    Added EuclideanDistance function

--- Pathfinder.cpp
+++ Pathfinder.cpp
@@ -5,6 +5,27 @@ std::vector<Point> Pathfinder::findPath(const Map& map, const Point& start, cons
 {
 	// TODO: implement the A* algorithm to find a path from start to goal
 
+	std::vector<Point> closedSet;
+
+	std::vector<Point> openSet = {start};
+
+
 	std::vector<Point> result;
 	return result;
 }
+
+
+std::vector<Point> Pathfinder::reconstructPath(const Point& goalNode)
+{
+
+}
+
+
+int EuclideanDistance(Point currentNode, Point nextNode) //Change names
+{
+	double processX = (nextNode.getX - currentNode.getX) * 2;
+	double processY = (nextNode.getY - currentNode.getY) * 2;
+	double distance = sqrt(processX + processY);
+	return distance;
+}
+
--- Pathfinder.h
+++ Pathfinder.h
@@ -6,4 +6,5 @@ class Pathfinder
 {
 public:
 	std::vector<Point> findPath(const Map& map, const Point& start, const Point& goal);
+	std::vector<Point> Pathfinder::reconstructPath(const Point& goalNode);
 };
